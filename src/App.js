59e70ccfecd37f5b24b5d75b8a38a275448fba07
laiba ahsan <laiba.ahsan27@gmail.com>
2023-06-27 03:33:33 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css'
 import MeetTheTeam from './components/MeetTheTeam'
 import RecentWork from './components/RecentWork'
 import ReadyTo from './components/ReadyTo'
+import Navbar from './components/Navbar'
 
 
 const App = () => {
   return (
     <>
+      <Navbar/>
       <MeetTheTeam/>
       <RecentWork/>
       <ReadyTo/>
